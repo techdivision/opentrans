3d4d3faa968b4148cf1dd36405463d01d4a01301
Vadim Justus <v.justus@techdivision.com>
2021-01-07 16:03:12 +0100

    Avoid inheritance from BMECat party for party node

--- src/Nodes/Party.php
+++ src/Nodes/Party.php
@@ -4,11 +4,12 @@ namespace Naugrim\OpenTrans\Nodes;
 
 use /** @noinspection PhpUnusedAliasInspection */
     JMS\Serializer\Annotation as Serializer;
+use Naugrim\BMEcat\Nodes\Address;
 use Naugrim\BMEcat\Nodes\Contracts\NodeInterface;
-use Naugrim\BMEcat\Nodes\Party as BmeParty;
+use Naugrim\BMEcat\Nodes\Mime;
 use Naugrim\OpenTrans\Nodes\Party\Account;
 
-class Party extends BmeParty implements NodeInterface
+class Party implements NodeInterface
 {
     /**
      * @Serializer\Expose
@@ -19,7 +20,6 @@ class Party extends BmeParty implements NodeInterface
      */
     protected $id;
 
-
     /**
      * @Serializer\Expose
      * @Serializer\Type("Naugrim\OpenTrans\Nodes\Party\Account")
@@ -28,6 +28,35 @@ class Party extends BmeParty implements NodeInterface
      * @var Account
      */
     protected $account;
+
+    /**
+     * @Serializer\Expose
+     * @Serializer\Type("string")
+     * @Serializer\SerializedName("bme:PARTY_ROLE")
+     *
+     * @var string
+     */
+    protected $role;
+
+    /**
+     * @Serializer\Expose
+     * @Serializer\Type("Naugrim\BMEcat\Nodes\Address")
+     * @Serializer\SerializedName("bme:ADDRESS")
+     *
+     * @var Address
+     */
+    protected $address;
+
+    /**
+     *
+     * @Serializer\Expose
+     * @Serializer\SerializedName("bme:MIME_INFO")
+     * @Serializer\Type("array<Naugrim\BMEcat\Nodes\Mime>")
+     * @Serializer\XmlList( entry="MIME")
+     *
+     * @var Mime[]
+     */
+    protected $mimes = [];
 
     /**
      * @return string
@@ -64,5 +93,59 @@ class Party extends BmeParty implements NodeInterface
     {
         $this->account = $account;
         return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param string $role
+     * @return Party
+     */
+    public function setRole(string $role): Party
+    {
+        $this->role = $role;
+        return $this;
+    }
+
+    /**
+     * @param Address $address
+     * @return Party
+     */
+    public function setAddress(Address $address): Party
+    {
+        $this->address = $address;
+        return $this;
+    }
+
+    /**
+     * @return Address
+     */
+    public function getAddress(): Address
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param Mime[] $mimes
+     * @return Party
+     */
+    public function setMimes(array $mimes): Party
+    {
+        $this->mimes = $mimes;
+        return $this;
+    }
+
+    /**
+     * @return Mime[]
+     */
+    public function getMimes(): array
+    {
+        return $this->mimes;
     }
 }
